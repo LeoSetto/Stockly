670fae4ae6329cf2be09d1257a2fd47803a3cb91
LeoSetto <leosetto2020@gmail.com>
2026-03-28 16:08:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,17 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      {({ user, logout, saveUserData, loadUserData }) => (
-        <App user={user} logout={logout} saveUserData={saveUserData} loadUserData={loadUserData} />
+      {({ user, logout, saveUserData, loadUserData, houseCode, houseInfo, leaveHouse, refreshHouseInfo }) => (
+        <App
+          user={user}
+          logout={logout}
+          saveUserData={saveUserData}
+          loadUserData={loadUserData}
+          houseCode={houseCode}
+          houseInfo={houseInfo}
+          leaveHouse={leaveHouse}
+          refreshHouseInfo={refreshHouseInfo}
+        />
       )}
     </AuthProvider>
   </React.StrictMode>
