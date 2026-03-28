14f0eba19fa55b8341ecb346032e15902f89d72a
LeoSetto <leosetto2020@gmail.com>
2026-03-28 10:28:26 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AuthProvider from "./AuthProvider";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      {({ user, logout, saveUserData, loadUserData }) => (
+        <App user={user} logout={logout} saveUserData={saveUserData} loadUserData={loadUserData} />
+      )}
+    </AuthProvider>
   </React.StrictMode>
 );
